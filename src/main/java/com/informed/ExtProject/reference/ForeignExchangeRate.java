94db900ce8a1b19f0b8680486c3890fc3c30364a
kggera <kieren.gera@gmail.com>
2022-11-07 09:51:44 +0000

    Adding new Foreign Exchange functionality to RefDataService.

--- src/main/java/com/informed/ExtProject/reference/ForeignExchangeRate.java
+++ src/main/java/com/informed/ExtProject/reference/ForeignExchangeRate.java
@@ -1,14 +1,14 @@
 package com.informed.ExtProject.reference;
 
-public class ForeignExchangeRates {
+public class ForeignExchangeRate {
 
-    public Currency origin_currency;
-    public Currency destination_currency;
-    public double exchange_rate;
+    public Currency originCurrency;
+    public Currency destinationCurrency;
+    public double exchangeRate;
 
-    public ForeignExchangeRates(Currency origin_currency, Currency destination_currency, double exchange_rate) {
-        this.origin_currency = origin_currency;
-        this.destination_currency = destination_currency;
-        this.exchange_rate = exchange_rate;
+    public ForeignExchangeRate(Currency originCurrency, Currency destinationCurrency, double exchangeRate) {
+        this.originCurrency = originCurrency;
+        this.destinationCurrency = destinationCurrency;
+        this.exchangeRate = exchangeRate;
     }
 }
