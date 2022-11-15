e4d0f0adbabd22abd681ebbf584a8c9517223267
Kit <kit.boothroyd@informed.com>
2022-11-15 12:07:25 +0000

    delete functionality started

--- src/main/resources/public/js/counterparty-table-script.js
+++ src/main/resources/public/js/counterparty-table-script.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
         table += "<th>Phone Number</th>";
         table += "<th>Email Address</th>";
         table += "<th>Postcode</th>";
+        table += "<th></th>";
         table += "</tr>";
         counterparties.forEach(function (counterparty) {
             let tr = "<tr>";
@@ -15,6 +16,8 @@ $(document).ready(function() {
             tr += "<td>" + counterparty.phoneNumber + "</td>";
             tr += "<td>" + counterparty.emailAddress + "</td>";
             tr += "<td>" + counterparty.address.postcode + "</td>";
+            let id = counterparty.id;
+            tr += '<td><button onclick="deleteCounterParty('+ id + ')">Delete</button></td>';
             tr += "</tr>";
             table += tr;
             });
@@ -23,3 +26,9 @@ $(document).ready(function() {
         $("#counterparties").append($(html));
     });
 });
+
+function deleteCounterParty(id) {
+    console.log("Delete counter party called on counter party with id: " + id);
+    let deleteString = "http://localhost:8084/trader/counterparties/"+id;
+    console.log(deleteString);
+}
