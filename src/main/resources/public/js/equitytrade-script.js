dcf745e6f507c57be343958344b1a2fee13c7509
kggera <kieren.gera@gmail.com>
2022-11-15 17:49:59 +0000

    Updating equity trade web page.

--- src/main/resources/public/js/equitytrade-script.js
+++ src/main/resources/public/js/equitytrade-script.js
@@ -1,5 +1,3 @@
-
-
 function showEquityTrades() {
     console.log("Clicked 'View equity trades'.")
     $.get("http://localhost:8084/trader/equitytrades/list",
@@ -7,20 +5,46 @@ function showEquityTrades() {
             $("#equitytrades").empty();
             console.log(equitytrades);
 
-            let html = "<div class='equitytrades'";
+            let html = "<div id='equitytrades'>";
             $.each(equitytrades, function(i, equitytrade) {
                 console.log(equitytrade);
-                html += "Counter Party 1: " + equitytrade.CounterParty1;
-                html += "<br />Counter Party 2: " + equitytrade.counterParty2.name;
-                html += "<br />Agreement date: " + equitytrade.agreementDate;
-                html += "<br />Equity: " + equitytrade.equity.name;
-                html += "<br />Amount: " + equitytrade.amount;
-                html += "<br />Price: " + equitytrade.price;
-                html += "<br />Currency: " + equitytrade.currency.symbol;
-                html += "<br />Exchange: " + equitytrade.exchange.name;
-            })
+                html += "<h3 class='title'>Equity trade " + equitytrade.id + ": </h3>";
+                html += "<p>";
+                html += "Counter Party 1: " + equitytrade.counterParty1.name + "<br>";
+                html += "Counter Party 2: " + equitytrade.counterParty2.name + "<br>";
+                html += "Agreement date: " + equitytrade.agreementDate + "<br>";
+                html += "Equity: " + equitytrade.equity.name + "<br>";
+                html += "Amount: " + equitytrade.amount + "<br>";
+                html += "Price: " + equitytrade.price + "<br>";
+                html += "Currency: " + equitytrade.currency.symbol + "<br>";
+                html += "Exchange: " + equitytrade.exchange.name;
+                html += "</p>";
+            });
             html += "</div";
-            $("equitytrades").append($(html));
+            $("#equitytrades").append($(html));
     });
     console.log("Finished showing equity trades.");
 }
+
+function addEquityTrade() {
+  const form = document.getElementById('form');
+
+  if (form.style.display === 'block') {
+    form.style.display = 'none';
+  } else {
+    form.style.display = 'block';
+  }
+};
+
+
+    // let obj = $(this).serializeJSON();
+    // let data = JSON.stringify(obj);
+
+    // $.ajax({
+    // type: "POST",
+    // url: "http://localhost:8082/bookshop/addresses",
+    // data: data,
+    // contentType: "application/json; charset=utf-8",
+    // dataType: "json"
+    // }
+    // );
